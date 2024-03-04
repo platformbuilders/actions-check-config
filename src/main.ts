6bc78202fa7c2a63bc57c90bd0e2d3ef8908f067
Lucas <md.lucasd@gmail.com>
2024-03-04 12:15:14 -0300

    added comment PR

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import { glob } from 'glob'
 import { promises as fsPromises } from 'fs'
 
 export async function run(): Promise<void> {
   try {
     const fileEnv: string = core.getInput('env_path', { required: true }).trim()
-
+    const github_token = core.getInput('GITHUB_TOKEN')
     let configCheck: boolean = false
 
     // Check in parallel
@@ -19,11 +20,28 @@ export async function run(): Promise<void> {
         configCheck = !result
       })
     }
+    const context = github.context
+    if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.')
+      return
+    }
+    const pull_request_number = context.payload.pull_request.number
+    const octokit = github.getOctokit(github_token)
 
     if (!configCheck) {
       core.info(`O codepush não está configurado`)
       core.setOutput('is_configured', 'false')
+      await octokit.rest.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request_number as number,
+        body: 'O codepush não está configurado'
+      })
     } else {
+      await octokit.rest.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request_number as number,
+        body: '🎉 Codepush está configurado'
+      })
       core.info('🎉 Codepush está configurado')
       core.setOutput('is_configured', 'true')
     }
